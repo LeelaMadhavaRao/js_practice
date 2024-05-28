81c6f1e0d6d4448e890e98992d95d1db828cf348
Davasam Karthikeya <davasamkarthikeya@gmail.com>
2024-05-29 00:18:21 +0530

    Currency Converter Is Ready

---
+++ currency_con.js
@@ -0,0 +1,39 @@
+var CURR_DATA = {};
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_EdklOlJhENKwyhFlklTcCJbGqxoNOrD8qwoHPIFi&currencies=USD%2CINR%2CJPY%2CEUR%2CNZD');
+xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+        CURR_DATA = JSON.parse(xhr.responseText).data;
+        console.log(CURR_DATA)
+    } else {
+        console.error('Request failed with status', xhr.status);
+    }
+};
+xhr.send();
+function Convert1(){
+    var in1 = Number(document.getElementById('input1').value)
+    var curr1 = document.getElementById('first').value
+    var curr2 = document.getElementById('second').value
+    
+    if(curr1 == curr2){
+        document.getElementById('input2').value = in1.toFixed(4);
+        return;
+    }
+
+    in2 = (in1 * (1/CURR_DATA[curr1])) * CURR_DATA[curr2];
+    document.getElementById('input2').value = in2.toFixed(4);
+
+}
+function Convert2(){
+    var in2 = document.getElementById('input2').value
+    var curr1 = document.getElementById('first').value
+    var curr2 = document.getElementById('second').value
+    
+    if(curr1 == curr2){
+        document.getElementById('input1').value = in2.toFixed(4);
+        return;
+    }
+
+    in1 = (in2 * (1/CURR_DATA[curr2])) * CURR_DATA[curr1];
+    document.getElementById('input1').value = in1.toFixed(4);
+}
