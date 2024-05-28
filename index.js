67291d640e26c326164c0eb84d8e38f6a3b954b7
LeelaMadhavaRao <leelamadhav.nulakani@gmail>
2024-05-28 20:11:32 +0530

    added files

---
+++ index.js
@@ -0,0 +1,197 @@
+//ground around problem
+
+/*print(1,1,5);
+
+function print(per,start,end){
+        for(let i=start;i<=end;i++)
+          {
+             console.log(`person ${per} completed round ${i}`);
+          }
+}*/
+
+
+
+
+
+//color change
+
+/*function colortext(ch){
+        var p=document.getElementById("text")
+        p.style.color=`${ch}`;
+        p.textContent=`The color is ${ch}`;
+}   */
+
+
+
+
+//color changer-2
+
+/*function backgroundcol(col){
+    
+    switch(col)
+    {
+        case 1 :var p=document.getElementById("div1")
+                p.style.backgroundColor="red";
+                break;
+        case 2 :var p=document.getElementById("div1")
+                p.style.backgroundColor="blue";
+                break;
+        case 3 :var p=document.getElementById("div1")
+                p.style.backgroundColor="green";
+                break;
+        case 4 :var p=document.getElementById("div1")
+                p.style.backgroundColor="yellow";
+                break;
+        case 5 : var p=document.getElementById("div1")
+                p.style.backgroundColor="orange";
+                break;
+        case 6 :var p=document.getElementById("div2")
+                p.style.backgroundColor="purple";
+                break;
+        case 7 : var p=document.getElementById("div2")
+                p.style.backgroundColor="pink";
+                break;
+        case 8 : var p=document.getElementById("div2")
+                p.style.backgroundColor="brown";
+                break;
+        case 9 : var p=document.getElementById("div2")
+                p.style.backgroundColor="black";
+                break;
+        case 10 :var p=document.getElementById("div2")
+                p.style.backgroundColor="white";
+                break;
+        case 11 :var p=document.getElementById("div3")
+                p.style.backgroundColor="gray";
+                break;
+        case 12 :var p=document.getElementById("div3")
+                p.style.backgroundColor="violet";
+                break;
+        case 13 :var p=document.getElementById("div3")
+                p.style.backgroundColor="indigo";
+                break;
+        case 14 :var p=document.getElementById("div3")
+                p.style.backgroundColor="cyan";
+                break;
+        case 15 :var p=document.getElementById("div3")
+                 p.style.backgroundColor="magenta";
+                break;
+        case 16 :var p=document.getElementById("div4")
+                 p.style.backgroundColor="lime";
+                 break;
+        case 17 :var p=document.getElementById("div4")
+                 p.style.backgroundColor="teal";
+                 break;
+        case 18 :var p=document.getElementById("div4")
+                 p.style.backgroundColor="maroon";
+                 break;
+        case 19 :var p=document.getElementById("div4")
+                 p.style.backgroundColor="navy";
+                 break;
+        case 20 :var p=document.getElementById("div4")
+                 p.style.backgroundColor="olive";
+                 break;
+        case 21 :var p=document.getElementById("div5")
+                 p.style.backgroundColor="aqua";
+                 break;
+        case 22 :var p=document.getElementById("div5")
+                 p.style.backgroundColor="coral";
+                 break;
+        case 23 :var p=document.getElementById("div5")
+                 p.style.backgroundColor="gold";
+                 break;
+        case 24 :var p=document.getElementById("div5")
+                 p.style.backgroundColor="silver";
+                 break;
+        case 25 :var p=document.getElementById("div5")
+                 p.style.backgroundColor="turquoise";
+                 break;
+    }
+}*/
+
+
+
+
+
+//cuboid volume calculator
+
+/*function areacal(){
+    var l=document.getElementById("len").value;
+    var b=document.getElementById("wid").value;
+    var h=document.getElementById("hgh").value;
+    var p=document.getElementById("area")
+    if(l=="" && b=="" && h=="")
+        alert('enter valid length ,breadth and height')
+    else if(l=="" && b=="")
+        alert('enter valid length and breadth')
+    else if(b=="" && h=="")
+        alert('enter valid breadth and height')
+    else if(l=="" && h=="")
+        alert('enter valid length and height')
+    else if(b=="")
+        alert('enter valid breadth')
+    else if( h=="")
+        alert('enter valid height')
+    else if(l=="")
+        alert('enter valid length')
+    else{
+        var x=2*((l*b)+(b*h)+(h*l));
+        p.textContent=`the surface area of cuboid is ${x}`;
+    }
+    
+}*/ 
+
+
+
+
+//fav food
+/*
+function imagedisplays(ch){
+   var p=document.getElementById("images")
+   switch(ch)
+   {
+        case 1 : p.src="https://www.southernliving.com/thmb/3x3cJaiOvQ8-3YxtMQX0vvh1hQw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/2652401_QFSSL_SupremePizza_00072-d910a935ba7d448e8c7545a963ed7101.jpg"
+                 break;
+        case 2 : p.src="https://assets.epicurious.com/photos/5c745a108918ee7ab68daf79/1:1/w_2560%2Cc_limit/Smashburger-recipe-120219.jpg"
+                 break;
+        case 3 : p.src="https://thecozycook.com/wp-content/uploads/2020/02/Copycat-McDonalds-French-Fries--500x375.jpg"
+                 break;
+        case 4 : p.src="https://geekrobocook.com/wp-content/uploads/2021/04/Chilly-Chicken.jpg"
+                 break;
+        case 5 : p.src="https://www.awesomecuisine.com/wp-content/uploads/2007/11/Pani-Puri.jpg"
+                 break;
+        case 6 : p.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTAlyLK84vptExS-XPSjIcEaqbGnZOQySNH1cL0c_pFg&s"
+                 break;
+   }
+}*/
+
+
+//create a webpage which performs task like converting currency 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
